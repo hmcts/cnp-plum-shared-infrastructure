ac183a92f9fd6c8eb54cfc476e63c7917b1a10f8
Rafal Hollins <rafalhollins@hotmail.com>
2019-01-23 11:41:41 +0000

    adding legacy endpoint on tm

--- trafficman.tf
+++ trafficman.tf
@@ -53,3 +53,20 @@ resource "azurerm_template_deployment" "traffic_manager" {
   deployment_mode     = "Incremental"
   parameters_body     = "${data.template_file.traffic_manager_parameters.rendered}"
 }
+
+
+data "null_data_source" "waf-pip" {
+  inputs = {
+    pip = "${module.waf.public_ip_fqdn}"     
+  }
+  depends_on = ["module.waf"]
+}
+
+resource "azurerm_traffic_manager_endpoint" "endpoint" {
+  name                = "${local.backend_name}"
+  resource_group_name = "${var.product}-shared-infrastructure-${var.env}"
+  profile_name        = "${var.product}-${var.env}"
+  target              = "${data.null_data_source.waf-pip.outputs["pip"]}"
+  type                = "externalEndpoints"
+  weight              = 100
+}
