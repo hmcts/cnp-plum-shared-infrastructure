a7f527fa72cd534a45cafb0b47c5071104616b62
danielev04 <daniele@vottero.eu>
2019-02-18 15:12:48 +0000

    null_data_source removed

--- deploymentTarget/trafficman.tf
+++ deploymentTarget/trafficman.tf
@@ -2,19 +2,11 @@
 module "trafficManagerProfileEndpoint" {
   source                  = "git@github.com:hmcts/cnp-module-trafficmanager-endpoint?ref=master"
   backend_name            = "${local.backend_name}"
-  backend_pip             = "${data.null_data_source.waf-pip.outputs["pip"]}"
+  backend_pip             = "${module.waf.public_ip_fqdn}" 
   public_hostname         = "${var.public_hostname}"
   priority                = "3"   // 1 = shutter-page, 2 = legacy, 3 = DT
   product                 = "${var.product}"
   env                     = "${var.env}"
   common_tags             = "${var.common_tags}"
   resource_group          = "${var.product}-shared-infrastructure-${var.env}"
-}
-
-
-data "null_data_source" "waf-pip" {
-  inputs = {
-    pip = "${module.waf.public_ip_fqdn}"     
-  }
-  depends_on = ["module.waf"]
 }
--- trafficman.tf
+++ trafficman.tf
@@ -1,18 +1,10 @@
 module "trafficManagerProfile" {
   source                  = "git@github.com:hmcts/cnp-module-trafficmanager?ref=master"
   backend_name            = "${local.backend_name}"
-  backend_pip             = "${data.null_data_source.waf-pip.outputs["pip"]}"
+  backend_pip             = "${module.waf.public_ip_fqdn}" 
   public_hostname         = "${var.public_hostname}"
   product                 = "${var.product}"
   env                     = "${var.env}"
   common_tags             = "${var.common_tags}"
   resource_group          = "${azurerm_resource_group.shared_resource_group.name}"
-}
-
-
-data "null_data_source" "waf-pip" {
-  inputs = {
-    pip = "${module.waf.public_ip_fqdn}"     
-  }
-  depends_on = ["module.waf"]
 }
