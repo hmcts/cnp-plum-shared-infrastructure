3583b5dddb2aa6f76dfc699648300ca79818c9ff
danielev04 <daniele@vottero.eu>
2019-02-18 10:36:15 +0000

    added traffic manager endpoint module

--- deploymentTarget/trafficman.tf
+++ deploymentTarget/trafficman.tf
@@ -1,48 +1,61 @@
-locals {
-  traffic_manager_endpoints = [
-    {
-      name        = "${local.backend_name}"
-      target      = "${data.null_data_source.waf-pip.outputs["pip"]}"
-      priority    = "3"
-      status      = "Enabled"
-      host_header = "${var.public_hostname}" // // This has to be the same hostname used in the listeners of the WAF
-    }
-  ]
+
+module "trafficManagerProfileEndpoint" {
+  source                  = "git@github.com:hmcts/cnp-module-trafficmanager-endpoint?ref=cnp-1180"
+  backend_name            = "${local.backend_name}"
+  backend_pip             = "${data.null_data_source.waf-pip.outputs["pip"]}"
+  public_hostname         = "${var.public_hostname}"
+  priority                = "3"
+  product                 = "${var.product}"
+  env                     = "${var.env}"
+  common_tags             = "${var.common_tags}"
+  resource_group          = "${azurerm_resource_group.shared_resource_group.name}"
 }
 
-data "template_file" "endpoints" {
-  template = "${file("${path.module}/templates/traffic-manager-endpoint.tpl")}"
-  count    = "1"
+# locals {
+#   traffic_manager_endpoints = [
+#     {
+#       name        = "${local.backend_name}"
+#       target      = "${data.null_data_source.waf-pip.outputs["pip"]}"
+#       priority    = "3"
+#       status      = "Enabled"
+#       host_header = "${var.public_hostname}" // // This has to be the same hostname used in the listeners of the WAF
+#     }
+#   ]
+# }
 
-  vars {
-    name        = "${lookup(local.traffic_manager_endpoints[count.index], "name")}"
-    target      = "${lookup(local.traffic_manager_endpoints[count.index], "target")}"
-    priority    = "${lookup(local.traffic_manager_endpoints[count.index], "priority")}"
-    status      = "${lookup(local.traffic_manager_endpoints[count.index], "status")}"
-    host_header = "${lookup(local.traffic_manager_endpoints[count.index], "host_header")}"
-  }
-}
+# data "template_file" "endpoints" {
+#   template = "${file("${path.module}/templates/traffic-manager-endpoint.tpl")}"
+#   count    = "1"
 
-data "template_file" "traffic_manager_parameters" {
-  template = "${file("${path.module}/templates/traffic-manager-parameters.tpl")}"
+#   vars {
+#     name        = "${lookup(local.traffic_manager_endpoints[count.index], "name")}"
+#     target      = "${lookup(local.traffic_manager_endpoints[count.index], "target")}"
+#     priority    = "${lookup(local.traffic_manager_endpoints[count.index], "priority")}"
+#     status      = "${lookup(local.traffic_manager_endpoints[count.index], "status")}"
+#     host_header = "${lookup(local.traffic_manager_endpoints[count.index], "host_header")}"
+#   }
+# }
 
-  vars {
-    name      = "${var.product}-${var.env}"
-    endpoints = "${join(",", data.template_file.endpoints.*.rendered)}"
-  }
-}
+# data "template_file" "traffic_manager_parameters" {
+#   template = "${file("${path.module}/templates/traffic-manager-parameters.tpl")}"
 
-data "template_file" "traffic_manager_template" {
-  template = "${file("${path.module}/templates/tm-endpoint.json")}"
-}
+#   vars {
+#     name      = "${var.product}-${var.env}"
+#     endpoints = "${join(",", data.template_file.endpoints.*.rendered)}"
+#   }
+# }
 
-resource "azurerm_template_deployment" "traffic_manager_enpoints" {
-  template_body       = "${data.template_file.traffic_manager_template.rendered}"
-  name                = "${var.product}-${var.env}-endpoints"
-  resource_group_name = "${var.product}-shared-infrastructure-${var.env}"
-  deployment_mode     = "Incremental"
-  parameters_body     = "${data.template_file.traffic_manager_parameters.rendered}"
-}
+# data "template_file" "traffic_manager_template" {
+#   template = "${file("${path.module}/templates/tm-endpoint.json")}"
+# }
+
+# resource "azurerm_template_deployment" "traffic_manager_enpoints" {
+#   template_body       = "${data.template_file.traffic_manager_template.rendered}"
+#   name                = "${var.product}-${var.env}-endpoints"
+#   resource_group_name = "${var.product}-shared-infrastructure-${var.env}"
+#   deployment_mode     = "Incremental"
+#   parameters_body     = "${data.template_file.traffic_manager_parameters.rendered}"
+# }
 
 data "null_data_source" "waf-pip" {
   inputs = {
@@ -50,3 +63,5 @@ data "null_data_source" "waf-pip" {
   }
   depends_on = ["module.waf"]
 }
+
+
