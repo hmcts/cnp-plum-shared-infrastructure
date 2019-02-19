bbb3bb3bf414d6a0b61f78cf811cc3e10c4f11c9
danielev04 <daniele@vottero.eu>
2019-02-19 16:10:22 +0000

    trafficManagerProfile passing parameter refactoring

--- trafficman.tf
+++ trafficman.tf
@@ -1,12 +1,30 @@
+
+
+locals {
+  traffic_manager_endpoints = [
+    {
+      name        = "shutter-page"
+      target      = "mojmaintenance.azurewebsites.net"
+      priority    = "1"
+      status      = "Disabled"
+      host_header = ""
+    },
+    {
+      name        = "${local.backend_name}"
+      target      = "${module.waf.public_ip_fqdn}" 
+      priority    = "2"
+      status      = "Enabled"
+      host_header = "${var.public_hostname}" // // This has to be the same hostname used in the listeners of the WAF
+    }
+  ]
+}
+
 module "trafficManagerProfile" {
-  source                  = "git@github.com:hmcts/cnp-module-trafficmanager?ref=master"
-  shutter_page_target     = "mojmaintenance.azurewebsites.net"
-  org_name                =  "hmcts"
-  backend_name            = "${local.backend_name}"
-  backend_pip             = "${module.waf.public_ip_fqdn}" 
-  public_hostname         = "${var.public_hostname}"
-  product                 = "${var.product}"
-  env                     = "${var.env}"
-  common_tags             = "${var.common_tags}"
-  resource_group          = "${azurerm_resource_group.shared_resource_group.name}"
+  source                    = "git@github.com:hmcts/cnp-module-trafficmanager?ref=cnp-1180"
+  org_name                  =  "hmcts"
+  product                   = "${var.product}"
+  env                       = "${var.env}"
+  common_tags               = "${var.common_tags}"
+  resource_group            = "${azurerm_resource_group.shared_resource_group.name}"
+  traffic_manager_endpoints = "${local.traffic_manager_endpoints}"
 }
