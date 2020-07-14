a591cc253ea80fb1a15c76a89ee23ae7c7f2fb3b
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-07-14 16:20:04 +0100

    Moving to latest terraform and azurerm

--- main.tf
+++ main.tf
@@ -1,27 +1,24 @@
-
-
-
 provider "azurerm" {
   
-  version = "=1.33.1"
+  version = "=2.18.0"
 }
 
 locals {
   vault_name = "${var.product}si-${var.env}"
   
-  tags = "${merge(
+  tags = merge(
     var.common_tags,
     map("Team Contact", var.team_contact)
-  )}"
+  )
 
 }
 
 //SHARED RESOURCE GROUP
 resource "azurerm_resource_group" "shared_resource_group" {
   name     = "${var.product}-shared-infrastructure-${var.env}"
-  location = "${var.location}"
+  location = var.location
 
-  tags = "${local.tags}"
+  tags = local.tags
 }
 
 
--- vault.tf
+++ vault.tf
@@ -1,18 +1,32 @@
 //KEY VAULT RESOURCE
 
+resource "azurerm_user_assigned_identity" "plum-identity" {
+
+  resource_group_name = "managed-identities-${var.env}-rg"
+  location            = var.location
+
+  name = "${var.product}-${var.env}-mi"
+
+  tags = var.common_tags
+}
+
 module "vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                    = "${local.vault_name}"
-  product                 = "${var.product}"
-  env                     = "${var.env}"
-  tenant_id               = "${var.tenant_id}"
-  object_id               = "${var.jenkins_AAD_objectId}"
-  resource_group_name     = "${azurerm_resource_group.shared_resource_group.name}"
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
+  name                    = local.vault_name
+  product                 = var.product
+  env                     = var.env
+  tenant_id               = var.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.shared_resource_group.name
   product_group_object_id = "300e771f-856c-45cc-b899-40d78281e9c1"
-  common_tags = "${local.tags}"
-  managed_identity_object_ids= ["${var.managed_identity_object_id}"]
+  common_tags = local.tags
+  managed_identity_object_ids= [azurerm_user_assigned_identity.plum-identity.principal_id,var.managed_identity_object_id]
 }
 
 output "vaultName" {
-  value = "${module.vault.key_vault_name}"
+  value = module.vault.key_vault_name
+}
+
+output "team_identity_client_id" {
+  value = azurerm_user_assigned_identity.plum-identity.client_id
 }
