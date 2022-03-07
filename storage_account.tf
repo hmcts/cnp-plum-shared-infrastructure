37653a6ea75d3e3dad37431a466564570f1dc51e
Ciarán Carragher <c.carragher@kainos.com>
2022-03-07 16:13:31 +0000

    Remove redundant subnets for testing

--- storage_account.tf
+++ storage_account.tf
@@ -9,13 +9,10 @@ locals {
   aks_core_vnet_rg = var.env == "preview" ? join("-", ["cft", var.env, "network-rg"]) : join("-", ["aks-infra", var.env, "rg"])
 
   sa_aat_subnets = [
-    //data.azurerm_subnet.jenkins_subnet.id,
-    data.azurerm_subnet.aks-mgmt.id,
     data.azurerm_subnet.aks-00-aat.id,
     data.azurerm_subnet.aks-01-aat.id]
 
   sa_prod_subnets = [
-    data.azurerm_subnet.aks-mgmt.id,
     data.azurerm_subnet.aks-00-prod.id,
     data.azurerm_subnet.aks-01-prod.id]
 
