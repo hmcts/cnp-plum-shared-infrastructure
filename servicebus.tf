7575cb9a81de5a1e41fbd710ca08969a7326f21f
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-06 11:34:08 +0100

    Update servicebus.tf

--- servicebus.tf
+++ servicebus.tf
@@ -16,7 +16,7 @@ module "servicebus-namespace" {
   name                    = local.servicebus_namespace_name
   location                = var.location
   resource_group_name     = azurerm_resource_group.shared_resource_group.name
-  env                     = var.env
+  env                     = local.env
   common_tags             = var.common_tags
   project                 = var.project
   capacity                = 1
