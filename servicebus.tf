21bf1eb20afaead4429e1e92a6560f27aaf7e83f
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-07 11:21:47 +0100

    DTSPO-6371 - revert to standard servicebus (#40)
    * DTSPO-6371 - revert to standard servicebus
    
    * Update servicebus.tf
    
    * Update servicebus.tf
    
    * Update servicebus.tf

--- servicebus.tf
+++ servicebus.tf
@@ -13,8 +13,5 @@ module "servicebus-namespace" {
   resource_group_name     = azurerm_resource_group.shared_resource_group.name
   env                     = var.env
   common_tags             = var.common_tags
-  project                 = var.project
-  capacity                = 1
-  enable_private_endpoint = true
   private_endpoint_subscription_id = var.aks_subscription_id
 }
