c964ac00cf556f29c7943dfe6475d6cacd403100
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-07 12:49:56 +0100

    update to branch with required providers

--- servicebus.tf
+++ servicebus.tf
@@ -10,11 +10,10 @@ module "servicebus-namespace" {
   providers = {
     azurerm.private_endpoint = azurerm.private_endpoint
   }
-  source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+  source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=DTSPO-6371_remove_provider"
   name                    = local.servicebus_namespace_name
   location                = var.location
   resource_group_name     = azurerm_resource_group.shared_resource_group.name
   env                     = var.env
   common_tags             = var.common_tags
-  private_endpoint_subscription_id = var.aks_subscription_id
 }
