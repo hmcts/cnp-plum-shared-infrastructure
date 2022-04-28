b1fe109ae505ddd0fdce4191f33c9df7ef6afd3e
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-28 15:03:20 +0100

    DTSPO-6371 - add provider for private endpoint (#39)
    * DTSPO-6371 - add provider for private endpoint
    
    * Update servicebus.tf
    
    * Remove mistake on source
    
    * Fix provider names
    
    * Fix alias name
    
    * Update servicebus.tf
    
    * Update servicebus.tf
    
    * Update servicebus.tf
    
    * Update servicebus.tf
    
    * Update servicebus.tf
    
    * Update servicebus.tf
    
    * Update servicebus.tf
    
    * DTSP-6371 - upgrade azurerm to 3.2.0

--- servicebus.tf
+++ servicebus.tf
@@ -7,11 +7,16 @@ locals {
 }
 
 module "servicebus-namespace" {
+  providers = {
+    azurerm.private_endpoint = azurerm.private_endpoint
+  }
+
   source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
   name                    = local.servicebus_namespace_name
   location                = var.location
   resource_group_name     = azurerm_resource_group.shared_resource_group.name
   env                     = var.env
   common_tags             = var.common_tags
+  
   private_endpoint_subscription_id = var.aks_subscription_id
 }
