45ec9dfe0d1cbf275c3e37be03db8436b0a5c981
Jennifer Strejevitch <jenniferstrej@gmail.com>
2022-05-31 10:45:22 +0100

    fix test

--- application-insights.tf
+++ application-insights.tf
@@ -4,7 +4,7 @@ resource "azurerm_application_insights" "appinsights" {
   resource_group_name = azurerm_resource_group.shared_resource_group.name
   application_type    = var.application_type
   tags                = {
-    Wrong = Example
+    Wrong = "Example"
   }
 
   lifecycle {
