3fb3b9c386dd01adaa4f9dc69f4b94ed620d4460
Jennifer Strejevitch <jenniferstrej@gmail.com>
2022-05-31 11:01:39 +0100

    terraform fmt

--- application-insights.tf
+++ application-insights.tf
@@ -3,7 +3,7 @@ resource "azurerm_application_insights" "appinsights" {
   location            = var.appinsights_location
   resource_group_name = azurerm_resource_group.shared_resource_group.name
   application_type    = var.application_type
-  tags                = {
+  tags = {
     Wrong = "Example"
   }
 
