db67a5fd120c8ac6b7ce9192610552f721bd23c5
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2023-09-22 09:37:34 +0100

    Add chart-tests mi (#108)

--- vault.tf
+++ vault.tf
@@ -12,8 +12,20 @@ module "vault" {
   common_tags                 = local.tags
   managed_identity_object_id = var.managed_identity_object_id
   create_managed_identity     = true
+  additional_managed_identities_access = var.additional_managed_identities_access
 }
 
 output "vaultName" {
   value = module.vault.key_vault_name
+}
+
+resource "azurerm_user_assigned_identity" "managed_identity" {
+
+  resource_group_name = "managed-identities-${var.env}-rg"
+  location            = var.location
+
+  name = "chart-tests-${var.env}-mi"
+
+  tags  = var.common_tags
+  count = var.create_chart_tests_mi ? 1 : 0
 }
