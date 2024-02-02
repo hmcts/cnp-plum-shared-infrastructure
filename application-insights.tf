f09ad344bb0e7352924125754df56ab1860069e3
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-02-02 10:00:10 +0000

    use default function key

--- application-insights.tf
+++ application-insights.tf
@@ -18,7 +18,7 @@ resource "azurerm_monitor_action_group" "action_group" {
   azure_function_receiver {
     function_app_resource_id = data.azurerm_windows_function_app.alerts.id
     function_name            = "httpTrigger"
-    http_trigger_url         = "https://${data.azurerm_windows_function_app.alerts.default_hostname}/api/httpTrigger?code=${data.azurerm_function_app_host_keys.host_keys.primary_key}"
+    http_trigger_url         = "https://${data.azurerm_windows_function_app.alerts.default_hostname}/api/httpTrigger?code=${data.azurerm_function_app_host_keys.host_keys.default_function_key}"
     name                     = "slack-alerts"
     use_common_alert_schema  = true
   }
