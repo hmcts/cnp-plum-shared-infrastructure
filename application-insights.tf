af85495a206542fbb05161dad6c2d19f0b238a8a
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-02-19 11:26:39 +0000

    Switch back to main branch after merging App insight brnach.

--- application-insights.tf
+++ application-insights.tf
@@ -1,5 +1,5 @@
 module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=DTSPO-16829-prepare"
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
 
   env     = var.env
   product = var.product
