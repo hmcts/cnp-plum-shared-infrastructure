5c1a6badb3729d4cb3ad097106447f13528a0077
Narinder.Doal <narinder.doal@justice.gov.uk>
2024-05-23 16:44:17 +0100

    testing new script changes

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ locals {
 }
 
 //SHARED RESOURCE GROUP
-            resource "azurerm_resource_group" "shared_resource_group" {
+resource "azurerm_resource_group" "shared_resource_group" {
   name     = "${var.product}-shared-infrastructure-${var.env}"
-  location = var.location
+   location = var.location
 
   tags = local.tags
 }
