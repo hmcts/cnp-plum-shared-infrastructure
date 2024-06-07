a870a27abdf94b84094038871c3059eb8fb2a820
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-06-07 14:50:57 +0100

    remove files, add bad formatting

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ locals {
 
 //SHARED RESOURCE GROUP
 resource "azurerm_resource_group" "shared_resource_group" {
-  name     = "${var.product}-shared-infrastructure-${var.env}"
+  name     =      "${var.product}-shared-infrastructure-${var.env}"
   location = var.location
 
   tags = local.tags
