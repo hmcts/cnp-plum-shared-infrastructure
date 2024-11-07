3ababa2943eeefd74f5d2922327525cfbcfdd4e9
149849 <93778408+hmcts-jenkins-cft[bot]@users.noreply.github.com>
2024-11-07 17:09:22 +0000

    Updating Terraform Formatting

--- main.tf
+++ main.tf
@@ -3,21 +3,21 @@ provider "azurerm" {
 }
 
 locals {
-  vault_name         = "${var.product}si-${var.env}"
+  vault_name = "${var.product}si-${var.env}"
 
-  tags     = merge(
+  tags = merge(
     var.common_tags,
-    tomap({ "Team Contact"         = var.team_contact })
+    tomap({ "Team Contact" = var.team_contact })
   )
 
 }
 
 //SHARED RESOURCE GROUP
 resource "azurerm_resource_group" "shared_resource_group" {
-  name           = "${var.product}-shared-infrastructure-${var.env}"
-  location               = var.location
+  name     = "${var.product}-shared-infrastructure-${var.env}"
+  location = var.location
 
-  tags       = local.tags
+  tags = local.tags
 }
 
 
