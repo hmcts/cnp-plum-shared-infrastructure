34cd8b75343443b5b12807a1e6b7f8e757e7b0c9
Ieuan Byers <ieuan.byers@justice.gov.uk>
2026-03-09 11:25:08 +0000

    Test webapp module

---
+++ webapp.tf
@@ -0,0 +1,51 @@
+module "frontend" {
+  source = "git@github.com:hmcts/cnp-module-webapp?ref=DTSPO-30615-modernise-module"
+
+  product             = var.product
+  env                 = var.env
+  webapp_name         = "plum-frontend"
+  resource_group_name = azurerm_resource_group.shared_resource_group.name
+  os_type             = "linux"
+  is_frontend         = true
+  service_plan_id     = ""
+
+  virtual_network_subnet_id = ""
+
+  docker_image_name   = "hello-world:latest"
+  docker_registry_url = "https://mcr.microsoft.com"
+
+  auth_client_id       = "00000000-0000-0000-0000-000000000000"
+  auth_tenant_endpoint = "https://login.microsoftonline.com/${var.tenant_id}/v2.0"
+
+  allowed_external_redirect_urls = [
+    "https://my-product-dev.example.com",
+    "https://my-product-dev.example.com/",
+  ]
+
+}
+
+module "backend" {
+  source = "git@github.com:hmcts/cnp-module-webapp?ref=DTSPO-30615-modernise-module"
+
+  product             = var.product
+  env                 = var.env
+  webapp_name         = "plum-backend"
+  resource_group_name = azurerm_resource_group.shared_resource_group.name
+  os_type             = "linux"
+  is_frontend         = false
+  service_plan_id     = ""
+
+  virtual_network_subnet_id = ""
+
+  docker_image_name   = "hello-world:latest"
+  docker_registry_url = "https://mcr.microsoft.com"
+
+  auth_client_id       = "00000000-0000-0000-0000-000000000000"
+  auth_tenant_endpoint = "https://login.microsoftonline.com/${var.tenant_id}/v2.0"
+
+  allowed_external_redirect_urls = [
+    "https://my-product-dev.example.com",
+    "https://my-product-dev.example.com/",
+  ]
+
+}
