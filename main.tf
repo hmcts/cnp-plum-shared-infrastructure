8ae84eafbcb36ec801b44cf70dbafef30dca97d0
manohar.nunna <manohar.nunna@hmcts.net>
2026-04-27 13:14:23 +0100

    DTSPO-30499: Add jenkins managed identity keyvault access

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 locals {
   vault_name = "${var.product}si-${var.env}"
 
