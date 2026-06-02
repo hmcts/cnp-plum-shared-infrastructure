5ada49544eda2cc740fcfd0a9294df4b68256ee8
Nicholas Chang <nicholas.chang@justice.gov.uk>
2026-06-02 15:18:45 +0100

    add count

--- import.tf
+++ import.tf
@@ -1,4 +1,4 @@
 import {
-	to = module.vault.azurerm_key_vault_access_policy.creator_access_policy
+	to = module.vault.azurerm_key_vault_access_policy.creator_access_policy[0]
 	id = "/subscriptions/${var.subscription}/resourceGroups/${var.product}-shared-infrastructure-${var.env}/providers/Microsoft.KeyVault/vaults/${var.product}si-${var.env}/objectId/0292f26e-288e-4f5b-85fc-b99a53f0a2b1"
 }
