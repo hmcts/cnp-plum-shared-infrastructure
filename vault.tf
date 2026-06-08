e58a31761c130970ddb0a4e00af27972df5b6fd7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-06-08 16:29:48 +0100

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -6,7 +6,7 @@ module "vault" {
   product                              = var.product
   env                                  = var.env
   tenant_id                            = var.tenant_id
-  object_id                            = var.jenkins_AAD_objectId
+  object_id                            = data.azurerm_user_assigned_identity.cftsbox_jenkins.principal_id
   resource_group_name                  = azurerm_resource_group.shared_resource_group.name
   product_group_object_id              = "e7ea2042-4ced-45dd-8ae3-e051c6551789"
   common_tags                          = local.tags
