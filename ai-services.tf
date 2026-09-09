dcc7422f64d674be7f0c636aeb8227da6fc5d1c4
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-09 11:22:24 +0100

    undo fix to test

--- ai-services.tf
+++ ai-services.tf
@@ -47,7 +47,7 @@ module "ai_services" {
 module "document_intelligence" {
   count                           = var.env == "sandbox" ? 1 : 0
   existing_cognitive_account_name = "${var.product}-doc-intelligence-${var.env}"
-  source                          = "git@github.com:hmcts/terraform-module-ai-services?ref=main"
+  source                          = "github.com/hmcts/terraform-module-ai-services?ref=main"
 
   providers = {
     azurerm.private_dns = azurerm # required alias; unused since enable_managed_network = false skips all PE/DNS lookups
